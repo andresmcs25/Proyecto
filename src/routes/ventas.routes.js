964a41228713ae1a3b777c1599b07c93e1088e57
devAndre <andres.mcs1974@gmail.com>
2025-06-05 00:20:18 -0500

    Actualizacion Proyecto

--- src/routes/ventas.routes.js
+++ src/routes/ventas.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { isAuthenticated } from "../libs/isAuthenticated.js";
 import { renderVentas } from "../controllers/ventas.controller.js";
+import { generarFacturaPDF } from "../controllers/ventas.controller.js";
 
 const router = Router();
 
 router.get("/ventas", isAuthenticated, renderVentas);
+router.get("/ventas/pdf/:idVenta", generarFacturaPDF);
 
 export default router;
